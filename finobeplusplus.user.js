4cdd1c1e6c5250519d545cbea2622b38e87dd57e
jad <chehimj@gmail.com>
2017-05-17 18:58:49 -0600

    Update finobeplusplus.user.js

--- finobeplusplus.user.js
+++ finobeplusplus.user.js
@@ -1,12 +1,3 @@
-// ==UserScript==
-// @name         Finobe++
-// @namespace    jad#0875
-// @version      1.0
-// @description  Makes Finobe SEXY
-// @author       Jad C.
-// @match        https://fi.nobelium.xyz/*
-// @grant        none
-// @require http://code.jquery.com/jquery-3.2.1.js
-// ==/UserScript==
+!!!!!!!!!!!!!!   DON'T CLICK INSTALL! IT WONT DO ANYTHING!! !!!!!!!!!!!!!!!
 
-$("body").text("a");
+Finobe++ will come out soon.

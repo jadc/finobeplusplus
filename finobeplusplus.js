01583158395ecca38c84f124b940de9b2678b9e1
jad <chehimj@gmail.com>
2017-05-17 18:37:04 -0600

    Create finobeplusplus.js

---
+++ finobeplusplus.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Finobe++
+// @namespace    jad#0875
+// @version      1.0
+// @description  Makes Finobe SEXY
+// @author       Jad C.
+// @match        https://fi.nobelium.xyz/*
+// @grant        none
+// @require http://code.jquery.com/jquery-3.2.1.js
+// ==/UserScript==
+
+$("body").text("a");
